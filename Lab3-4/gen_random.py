06f570800ec844e579ca80e5ce24449ff2018d72
egorpodvig <151170262+egorpodvig@users.noreply.github.com>
2024-12-26 22:02:30 +0300

    Update gen_random.py

--- Lab3-4/gen_random.py
+++ Lab3-4/gen_random.py
@@ -1,1 +1,13 @@
+import random
+
+def gen_random(num_count, begin, end):
+    for i in range(num_count):
+        yield random.randint(begin, end)
+
+num_count = int(input())
+begin = int(input())
+end = int(input())
+
+for num in gen_random(num_count, begin, end):
+    print(num)
 
