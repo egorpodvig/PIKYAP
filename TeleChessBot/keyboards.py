44440baac9cb878cdb534476be03c8a05154bc69
egorpodvig <151170262+egorpodvig@users.noreply.github.com>
2024-12-26 22:05:52 +0300

    Update keyboards.py

--- TeleChessBot/keyboards.py
+++ TeleChessBot/keyboards.py
@@ -1,1 +1,13 @@
+from aiogram.types import (ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
 
+main = ReplyKeyboardMarkup(keyboard = [[KeyboardButton(text='Мат в 1 ход')],
+                                       [KeyboardButton(text='Мат в 2 хода'),
+                                       KeyboardButton(text='Мат в 4 хода')],
+                                       [KeyboardButton(text='Наименования фигур')]],
+                           input_field_placeholder='Выберете тип задачи:')
+
+rep = ReplyKeyboardMarkup(keyboard = [[KeyboardButton(text='Показать ответ'),
+                                       KeyboardButton(text='Наименования фигур')],
+                                      [KeyboardButton(text='Назад')]])
+back = ReplyKeyboardMarkup(keyboard = [[KeyboardButton(text='Назад')]])
