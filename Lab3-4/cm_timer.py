54654b480a89ee656824c34ffb64dacead0a856b
egorpodvig <151170262+egorpodvig@users.noreply.github.com>
2024-12-26 22:01:53 +0300

    Update cm_timer.py

--- Lab3-4/cm_timer.py
+++ Lab3-4/cm_timer.py
@@ -1,1 +1,29 @@
+import time
+from contextlib import contextmanager
+
+class cm_timer_1:
+    def __enter__(self):
+        self.start_time = time.time()
+        return self
+
+    def __exit__(self, а, d, h):
+        end_time = time.time()
+        elapsed_time = end_time - self.start_time
+        print(f"time: {elapsed_time:.2f}")
+
+@contextmanager
+def cm_timer_2():
+    start_time = time.time()
+    yield
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"time: {elapsed_time:.2f}")
+
+with cm_timer_1():
+    time.sleep(5.5)
+
+with cm_timer_2():
+    time.sleep(5.5)
+
+
 
