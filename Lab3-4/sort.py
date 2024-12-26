cdc3b14e7a05468a817323cd58f08b6f9f786d3b
egorpodvig <151170262+egorpodvig@users.noreply.github.com>
2024-12-26 22:03:15 +0300

    Update sort.py

--- Lab3-4/sort.py
+++ Lab3-4/sort.py
@@ -1,1 +1,9 @@
+data = [4, -30, 30, 100, -100, 123, 1, 0, -1, -4]
+
+if __name__ == '__main__':
+    result1 = sorted(data, key=abs, reverse=True)
+    print(result1)
+
+    result2 = sorted(data, key=lambda x: abs(x), reverse=True)
+    print(result2)
 
